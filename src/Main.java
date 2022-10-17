06d2f7af57ee7a40aee8f3429e83d8410bb7f1dd
anna-ermakova <korovanna8@mail.ru>
2022-10-17 19:30:07 +0300

    Задачи 1-4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,89 @@
+import java.util.Arrays;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание 1.");
+        //Задание 1
+        //Первым делом бухгалтеры попросили посчитать сумму всех выплат за месяц.
+        //Нужно написать программу, которая решит эту задачу, и вывести в консоль результат
+        //в формате: «Сумма трат за месяц составила … рублей».
+
+        int allPaymentsForTheMonth = 0;
+        int[] arr = generateRandomArray();
+        for (int value : arr) {
+            allPaymentsForTheMonth += value;
+        }
+        System.out.println("Сумма трат за месяц составила " + allPaymentsForTheMonth + "рублей");
+        System.out.println();
+
+        System.out.println("Задание 2.");
+
+        //Задание 2
+        //Следующая задача — найти минимальную и максимальную трату за день.
+        //Нужно написать программу, которая решит эту задачу, и вывести в консоль результат
+        //в формате: «Минимальная сумма трат за день составила … рублей. Максимальная сумма трат за день составила … рублей».
+
+
+        int[] arr2 = generateRandomArray();
+        int minPaymentsForTheMonth = arr2[0];
+        int maxPaymentsForTheMonth = arr2[0];
+        for (int element : arr2) {
+            if (element < minPaymentsForTheMonth) {
+                element = minPaymentsForTheMonth;
+            } else if (element > maxPaymentsForTheMonth) {
+                element = maxPaymentsForTheMonth;
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + minPaymentsForTheMonth + " рублей.");
+        System.out.println("Максимальная сумма трат за день составила " + maxPaymentsForTheMonth + " рублей.");
+        System.out.println();
+
+        System.out.println("Задание 3.");
+
+        //Задание 3
+        //А теперь нам нужно понять, какую в среднем сумму наша компания тратила в течение данных 30 дней.
+        //Нужно написать программу, которая посчитает среднее значение трат за месяц (то есть сумму всех трат за месяц поделить на количество дней), и вывести в консоль результат в формате: «Средняя сумма трат за месяц составила … рублей».
+        //**Важно помнить:** подсчет среднего значения может иметь остаток (то есть быть не целым, а дробным числом).
+
+        int sumPaymentsForTheMonth = 0;
+        int[] arr3 = generateRandomArray();
+        for (int value : arr3) {
+            sumPaymentsForTheMonth += value;
+
+            System.out.println("Средняя сумма трат за день составила " + (sumPaymentsForTheMonth / (float) arr3.length) + " рублей.");
+            break;
+        }
+        System.out.println();
+
+        System.out.println("Задание 4.");
+
+        //Задание 4
+        //Отойдем от подсчетов.
+        //В нашей бухгалтерской книге появился баг. Что-то пошло нет так, и Ф.И.О. сотрудников начали отображаться
+        //в обратную сторону. Т. е. вместо «Иванов Иван Иванович» мы имеем «чивонавИ навИ вонавИ».
+        //Данные с именами сотрудников хранятся в виде массива символов (char[]).
+        //Напишите код, который в случае такого бага будет выводить Ф. И. О. сотрудников в корректном виде. В качестве данных для массива используйте:
+        //char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        //В результате в консоль должно быть выведено "Ivanov Ivan".
+        //**Важно**: не используйте дополнительные массивы для решения этой задачи. Необходимо корректно пройти по массиву циклом и распечатать его элементы в правильном порядке.
+
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
     }
+
+    int[] arr = generateRandomArray();
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
 }
+
